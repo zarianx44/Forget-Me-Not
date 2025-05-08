fdcd7670d748bf68238d4d9e40e0df7f995b60b2
zarianx44 <zchau3@gmail.com>
2025-05-08 13:51:11 -0400

    mew

---
+++ Forget Me Not/CareView.swift
@@ -0,0 +1,8 @@
+//
+//  CareView.swift
+//  Forget Me Not
+//
+//  Created by Zara on 2025-05-07.
+//
+
+import Foundation
---
+++ Forget Me Not/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Forget Me Not
+//
+//  Created by Zara on 2025-05-07.
+//
+
+import Foundation
