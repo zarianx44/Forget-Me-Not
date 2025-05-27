e0da03b56d231b6c66169f5f06eb21b8491ea834
zarianx44 <zchau3@gmail.com>
2025-05-27 14:27:20 -0400

    hewgbiwarg

--- Forget Me Not/LocationManager.swift
+++ Forget Me Not/LocationManager.swift
@@ -1,43 +1,53 @@
-import Foundation
-import CoreLocation
 import MapKit
 
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    let locationManager = CLLocationManager()
+final class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
     
-    @Published var region: MKCoordinateRegion?
-    @Published var geofenceCenter: CLLocationCoordinate2D?
+    @Published var region = MKCoordinateRegion(
+        center: .init(latitude: 37.334_900, longitude: -122.009_020),
+        span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
     
     override init() {
         super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.setup()
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse || status == .authorizedAlways {
-            manager.startUpdatingLocation()
+    func setup() {
+        switch locationManager.authorizationStatus {
+        //If we are authorized then we request location just once, to center the map
+        case .authorizedWhenInUse:
+            locationManager.requestLocation()
+        //If we don´t, we request authorization
+        case .notDetermined:
+            locationManager.startUpdatingLocation()
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
         }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard .authorizedWhenInUse == manager.authorizationStatus else { return }
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Something went wrong: \(error)")
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        DispatchQueue.main.async {
-            self.region = MKCoordinateRegion(
-                center: location.coordinate,
-                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        locationManager.stopUpdatingLocation()
+        locations.last.map {
+            region = MKCoordinateRegion(
+                center: $0.coordinate,
+                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
             )
         }
-    }
-    
-    func setGeofence(center: CLLocationCoordinate2D) {
-        let region = CLCircularRegion(center: center, radius: 20, identifier: "MyGeofence")
-        region.notifyOnEntry = true
-        region.notifyOnExit = true
-        locationManager.startMonitoring(for: region)
-        geofenceCenter = center
-        print("✅ Geofence set at: \(center.latitude), \(center.longitude)")
     }
 }
